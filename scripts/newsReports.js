7e326e6ff96c26779a2a347bde8878c3712c984b
unknown <Steven@Lenny.kingsley.loc>
2026-01-08 13:32:01 +0000

    Create newsReports.js

---
+++ scripts/newsReports.js
@@ -0,0 +1,23 @@
+// news reports functions		2025
+
+function LoadSales ()	{
+	$.ajax({
+		type: 'POST',
+		url: 'ajax/AJAX_newsReports.cfm',
+		data: $('#srchForm').serialize(),
+		beforeSend:function(){
+			$("#resultDiv").empty(); // clear out old stuff
+			$("#resultDiv").hide(); 
+			$('#loadingDiv').html("<img src='images/loading_2.gif' class='loadingGif'>&nbsp;Loading...").fadeIn();
+		},
+		success:function(data){
+			$('#loadingDiv').html('&nbsp;');
+			$('#resultDiv').html(data).show();
+		},
+		error:function(data){
+			$('#resultDiv').html(data);
+			$('#loadingDiv').loading(false);
+		}
+	});				
+}
+
